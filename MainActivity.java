0aede71ffe4833359159e0b76582d6b6c0949732
lisongqi123 <95220171+lisongqi123@users.noreply.github.com>
2021-12-13 16:01:44 +0800

    滑动界面
    滑动界面

--- MainActivity.java
+++ MainActivity.java
@@ -1,113 +1,110 @@
-package com.example.tanchishe;
+package com.example.daohangbiaoqian;
 
-import static android.graphics.Color.BLACK;
-import static android.graphics.Color.YELLOW;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;//初始化滑动界面的控件
+    private PagerAdapter adapter;//该控件需要一个PagerAdapter
+    private ArrayList<View> views;//声明PagerAdapter的数据源
+    private int[] ids = {R.drawable.p1,R.drawable.p2,R.drawable.p3};//声明三张图片
+    private int [] tids = {R.id.tv_point1,R.id.tv_point2,R.id.tv_point3};
+    private  static final int POINT_ACTIVE = R.drawable.shape2;
+    private  static final int POINT_NORMAL = R.drawable.shape1;
+    private TextView []textViews;
 
-    int w;//宽
-    int h;//高
-    int i=0;
-    int j=0;
-    final static int TIME=500;//设置静态常量500毫秒（一分钟刷新两次）
-    TextView[][] textViews = new TextView[20][20];//声明一个20，20的二维数组用来存储格子
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        w=getScreenSize()[0];//获取手机屏幕宽
-        h=getScreenSize()[1];//0，1为下面return new int[] { screenWidth, screenHeigh };中数组的下标
-
-        LinearLayout linearLayout = new LinearLayout(this);//初始化最外面的linearlayout
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        setContentView(linearLayout);//最后一步，绑定视图（注意要放在初始化视图的后面）
-        for (int i = 0; i < 20; i++) {//设置垂直的20个LinearLayout
-            LinearLayout layout = new LinearLayout(this);//初始化垂直的linealayout
-            ViewGroup.LayoutParams p=new LinearLayout.LayoutParams(w,h/20);//动态设置控件宽高;宽拉满,高为1/20
-            layout.setLayoutParams(p);//将设置的p传入layout
-            for (int j = 0; j < 20; j++) {//设置水平的20个TextView
-                TextView textView = new TextView(this);
-                ViewGroup.LayoutParams p1=new LinearLayout.LayoutParams(w/20,h/20);//动态设置控件宽高;宽为1/20,高为1/20
-                textView.setLayoutParams(p1);//将设置的p1传入textView
-                //textView.setText("1");//显示内容
-                textView.setGravity(Gravity.CENTER);//设置居中
-                textViews[i][j]=textView;//将每一个textView存储到二维数组里面
-                layout.addView(textView);//将设置的文本内容添加到layout
-
-            }
-            linearLayout.addView(layout);//将20个垂直的Layout依次添加到最外面的linearLayout
+        setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.vp_welcome);//绑定viewPager的id
+        views = new ArrayList<>();//初始化ArrayList<View>
+        textViews = new TextView[3];
+        for (int i = 0; i < tids.length; i++) {
+            textViews[i] = findViewById(tids[i]);
         }
-        startAn();//开启动画线程
-
-
-
-
-    }
-    public int[] getScreenSize() {
-        DisplayMetrics dm = new DisplayMetrics();
-        // 获取屏幕信息
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int screenWidth = dm.widthPixels;
-        int screenHeigh = dm.heightPixels;
-
-        return new int[] { screenWidth, screenHeigh };
-    }
-    private void bianLiang(TextView textView){//创建一个变量的方法，并传入TextView
-        textView.setBackgroundColor(BLACK);//设置变化的颜色为黄色
-        //变了后为了变回来做一个延迟，需要使用线程
-        new Thread(new Runnable() {//括号中传入Runnable
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(TIME);//在子线程中设置沉睡，并抛异常
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                //在Android中，ui线程只能被ui线程修改，子线程不能直接更改ui线程，使用runOnUiThread,线程套线程
-                runOnUiThread(new Runnable() {//依旧传入new Runnable()
+        chageStu(0);
+        LayoutInflater inflater= getLayoutInflater();//将xml转化为view需要通过打气筒
+        for (int i = 0; i <ids.length ; i++) {//通过for循环打气并塞入数据
+            View view = inflater.inflate(R.layout.item,null);//选第二个
+            ImageView imageView =view.findViewById(R.id.iv_i);//接下来需要绑定图片和按钮
+            imageView.setImageResource(ids[i]);//绑定三张图片
+            if(i==2){//当第三张图片出现，设置按钮可见
+                Button button =view.findViewById(R.id.bt_go);//绑定按钮id
+                button.setVisibility(View.VISIBLE);//设置为可见
+                button.setOnClickListener(new View.OnClickListener() {//设置按钮的监听，传入new View.OnClickListener()
                     @Override
-                    public void run() {//在这里将颜色改回来
-                        textView.setBackgroundColor(Color.TRANSPARENT);//TRANSPARENT为将其改为父布局的颜色
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this,"正在开发中",Toast.LENGTH_SHORT).show();//内部内里面传入上下文需要加上类名
                     }
                 });
+
             }
-        }).start();
-    }
-    private void startAn(){//开启动画
-        new Thread(new Runnable() {
+            views.add(view);
+
+        }
+
+        adapter = new PagerAdapter() {
+            @Override//返回页卡条目
+            public int getCount() {
+                return views.size();
+            }
+
+            @Override//判断视图是否从对象中来
+            public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+                return view==object;
+            }
+
+            @NonNull//新增页卡,将指定的页卡添加到container，并将其返回
             @Override
-            public void run() {
-                while(true){//注意别忘记while循环
-                    try {
-                        Thread.sleep(TIME);//在子线程中设置沉睡，并抛异常
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }//开启延时
-                    i++;//由1到20从上往下亮(i为x，j为y)
-                    if(i>=20){//当i大于等于20的时候
-                        i=0;//将i还原成0
-                    }
-                    runOnUiThread(new Runnable() {//同上，子线程中不能修改主线程的ui
-                        @Override
-                        public void run() {
-                            bianLiang(textViews[i][10]);//调用变量方法，传入textViews
-                        }
-                    });
-                }
+            public Object instantiateItem(@NonNull ViewGroup container, int position) {
+                container.addView(views.get(position));
+                return views.get(position);
             }
-        }).start();
 
+            @Override//销毁页卡
+            public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+               // super.destroyItem(container, position, object);
+            }
+        };
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override//选中的时候
+            public void onPageSelected(int position) {
+                chageStu(position);//调用该方法
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
-
+    private void chageStu(int position){//创建一个改变状态的方法，传入高量
+        for (int i = 0; i <textViews.length ; i++) {
+            if(i==position){
+                textViews[i].setBackgroundResource(POINT_ACTIVE);
+            }else {
+                textViews[i].setBackgroundResource(POINT_NORMAL);
+            }
+        }
+    }
 }
